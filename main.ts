62ef7801d537017c4b087a48ada0045217dee2f3
pigplouf <103496137+pigplouf@users.noreply.github.com>
2022-04-20 10:20:48 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,26 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(min)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(max)
+    basic.clearScreen()
+})
+let min = 0
+let max = 0
+let currenttemperature = input.temperature()
+max = currenttemperature
+min = currenttemperature
+basic.forever(function () {
+    basic.showString(".")
+    currenttemperature = input.temperature()
+    if (currenttemperature < min) {
+        min = currenttemperature
+    }
+    if (currenttemperature > max) {
+        max = currenttemperature
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(1000)
+})
